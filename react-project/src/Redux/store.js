f4207980609fd2e33b61fe0957bc747571ad51ce
Ajay84sia <ajay9679339@gmail.com>
2023-03-28 19:10:00 +0530

    submit

---
+++ react-project/src/Redux/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import thunk from "thunk";
+import { reducer as productReducer } from './productReducer/reducer'
+const rootReducer = combineReducers({
+    productReducer,
+})
+
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
