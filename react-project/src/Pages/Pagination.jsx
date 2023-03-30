ccc26711b5f61bd904e786e8e7a0c942f7e93206
harshita <harshitakatara34>
2023-03-30 16:55:34 +0530

    done

--- react-project/src/Pages/Pagination.jsx
+++ react-project/src/Pages/Pagination.jsx
@@ -1,19 +1,29 @@
-import { useSelector } from "react-redux"
-import { Button } from "@chakra-ui/react"
+import { useSelector } from "react-redux";
+import { Button } from "@chakra-ui/react";
 
-export const Pagination=({page,setPage})=>{
-const{totalPage}=useSelector((store)=>store.productReducer)
-let allpages=Math.ceil(totalPage/10)
-let arr=new Array(allpages).fill(0)
-return(
-  <>
-    {arr.map((e,index)=>{
-      return(
-        <>
-        <Button style={{backgroundColor:page===(index+1)?"grey":"white", border:"1px solid black",textAlign:"center"}} onClick={()=>setPage(index+1)}>{index+1}</Button>
-        </>
-      )
-    })}
-  </>
-)
-}
+export const Pagination = ({ page, setPage }) => {
+  const { totalPage } = useSelector((store) => store.productReducer);
+  let allpages = Math.ceil(totalPage / 10);
+  let arr = new Array(allpages).fill(0);
+  return (
+    <>
+      {arr.map((e, index) => {
+        return (
+          < >
+            <Button
+              style={{
+                backgroundColor: page === index + 1 ? "grey" : "white",
+                border: "1px solid black",
+                textAlign: "center",
+              
+              }}
+              onClick={() => setPage(index + 1)}
+            >
+              {index + 1}
+            </Button>
+          </>
+        );
+      })}
+    </>
+  );
+};
--- react-project/src/Pages/Sidebar.jsx
+++ react-project/src/Pages/Sidebar.jsx
@@ -11,35 +11,31 @@ import { Box } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-const Sidebar = ({page,order}) => {
-  const[searchParams,setSearchParams]=useSearchParams()
-  const initialcategory=searchParams.getAll("category")
-  const[category,setcategory]=useState(initialcategory||[])
-//   const{products}=useSelector((store)=>store.productReducer)
- 
-// const[order,setOrder]=useState("")
-// const[reviews,setreviews]=useState("")
+const Sidebar = ({ page, order }) => {
+  const [searchParams, setsearchParams] = useSearchParams();
+  const initialcategory = searchParams.getAll("category");
+  const [category, setcategory] = useState(initialcategory || []);
+  const { products } = useSelector((store) => store.productReducer);
 
-useEffect(()=>{
-  let params={
-    category,
-  }
-order&& (params.order = order)
-  setSearchParams(params)
-},[order,category])
+  useEffect(() => {
+    let params = {
+      category,
+    };
+    order && (params.order = order);
+    setsearchParams(params);
+  }, [category, order]);
 
-const handleChange=(e)=>{
-  let newcategory=[...category]
-  const value=e.target.value;
-  console.log(value)
-  if(newcategory.includes(value)){
-    newcategory.splice(newcategory.indexOf(value),1)
-  }
-else{
-  newcategory.push(value)
-  }
-setcategory(newcategory)
-}
+  const handleChange = (e) => {
+    let newcategory = [...category];
+    const value = e.target.value;
+    console.log(value);
+    if (newcategory.includes(value)) {
+      newcategory.splice(newcategory.indexOf(value), 1);
+    } else {
+      newcategory.push(value);
+    }
+    setcategory(newcategory);
+  };
 
   return (
     <div style={{ height: "800px", width: "320px" }}>
@@ -51,7 +47,6 @@ setcategory(newcategory)
               height="50px"
               width="300px"
               backgroundColor="white"
-             
             >
               <Box as="span" flex="1" textAlign="left">
                 <h3>Availability</h3>
@@ -64,18 +59,12 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-           
               alignSelf="left"
             />
             <span>In Stock</span>
             <br />
             <br />
-            <Checkbox
-              height="20px"
-              width="20px"
-              textAlign="left"
-            
-            />
+            <Checkbox height="20px" width="20px" textAlign="left" />
             <span>Limited Stock</span>
           </AccordionPanel>
         </AccordionItem>
@@ -86,7 +75,7 @@ setcategory(newcategory)
               height="40px"
               width="300px"
               backgroundColor="white"
-              border="2px solid grey"
+              
             >
               <Box as="span" flex="1" textAlign="left">
                 <h3>Category</h3>
@@ -99,12 +88,12 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+           
               alignSelf="left"
               value={"tents"}
               onChange={handleChange}
               checked={category.includes("tents")}
-              defaultChecked={category=="tents"}
+              defaultChecked={category == "tents"}
             />
             <span>Tents</span>
             <br />
@@ -113,24 +102,38 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+           
               value={"Kitchen"}
               onChange={handleChange}
               checked={category.includes("Kitchen")}
             />
             <span>Kitchen</span>
+            <br />
+            <br />
+            <Checkbox
+              height="20px"
+              width="20px"
+              textAlign="left"
+            
+              checked={category.includes("Lighting")}
+              value={"Lighting"}
+              onChange={handleChange}
+            />
+            <span>lighting</span>
             <br/>
             <br/>
             <Checkbox
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
-              checked={category.includes("Lighting")}
-              value={"Lighting"}
+           
+              alignSelf="left"
+              value={"camp essentials"}
               onChange={handleChange}
+              checked={category.includes("camp essentials")}
+              defaultChecked={category == "camp essentials"}
             />
-            <span>lighting</span>
+            <span>Camp-Essentials</span>
           </AccordionPanel>
         </AccordionItem>
 
@@ -141,9 +144,9 @@ setcategory(newcategory)
               height="40px"
               width="300px"
               backgroundColor="white"
-              border="2px solid grey"
+              
             >
-              <Box as="span" flex="1" textAlign="left">
+               <Box as="span" flex="1" textAlign="left">
                 <h3>Type</h3>
               </Box>
               <AccordionIcon height="30px" width="30px" />
@@ -154,7 +157,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+            
               alignSelf="left"
             />
             <span>In Stock</span>
@@ -164,7 +167,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+             
             />
             <span>Limited Stock</span>
           </AccordionPanel>
@@ -176,7 +179,7 @@ setcategory(newcategory)
               height="40px"
               width="300px"
               backgroundColor="white"
-              border="2px solid grey"
+            
             >
               <Box as="span" flex="1" textAlign="left">
                 <h3>Brand</h3>
@@ -189,7 +192,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+             
               alignSelf="left"
             />
             <span>In Stock</span>
@@ -199,7 +202,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+           
             />
             <span>Limited Stock</span>
           </AccordionPanel>
@@ -211,7 +214,7 @@ setcategory(newcategory)
               height="40px"
               width="300px"
               backgroundColor="white"
-              border="2px solid grey"
+             
             >
               <Box as="span" flex="1" textAlign="left">
                 <h3>Price</h3>
@@ -224,7 +227,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+            
               alignSelf="left"
             />
             <span>Price:0-100</span>
@@ -234,31 +237,31 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+             
             />
             <span>Price:100-200</span>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <Checkbox
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+            
               alignSelf="left"
             />
             <span>Price:200-250</span>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <Checkbox
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+          
               alignSelf="left"
             />
             <span>Price:250-300</span>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <span>Price:300-above</span>
           </AccordionPanel>
         </AccordionItem>
@@ -269,7 +272,7 @@ setcategory(newcategory)
               height="40px"
               width="300px"
               backgroundColor="white"
-              border="2px solid grey"
+              
             >
               <Box as="span" flex="1" textAlign="left">
                 <h3>Average Ratings</h3>
@@ -282,7 +285,7 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+            
               alignSelf="left"
             />
             <span>In Stock</span>
@@ -292,14 +295,13 @@ setcategory(newcategory)
               height="20px"
               width="20px"
               textAlign="left"
-              border="1px solid black"
+             
             />
             <span>Limited Stock</span>
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
     </div>
-
   );
 };
 
